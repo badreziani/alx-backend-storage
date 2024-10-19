4675edde4317e6c4bbb06b5726b0977ebeea72f0
Badre Ziani <badreziani@gmail.com>
2024-10-19 20:15:21 +0100

    Update task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- Script that creates a table users with requisite requirements
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
